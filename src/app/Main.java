0227d5d692fc3208dde58ead004646b1008d7c7d
Viktoriia Shutilova <shytilovav@ukr.net>
2025-02-01 01:00:09 +0100

    Add converter miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,9 +2,21 @@ package app;
 
 public class Main {
 
+    private static final double CONV_TO_K =1.609;
+
     public static void main(String[] args) {
 
         System.out.println("Converter distance");
 
+        double miles = 8;
+        double km = milesToKilometers(miles);
+
+        System.out.println(miles + " миль = " + km + " км");
+
     }
+
+    private static double milesToKilometers(double miles) {
+        return miles * CONV_TO_K;
+    }
+
 }
